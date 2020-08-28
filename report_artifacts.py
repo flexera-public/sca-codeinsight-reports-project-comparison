463e5cc20e57953c09fed0669e4233b50fd48351
Sean Geary <sgeary@flexera.com>
2020-08-28 16:15:16 -0400

    Add report content

--- report_artifacts.py
+++ report_artifacts.py
@@ -39,8 +39,6 @@ def generate_html_report(reportData):
     reportName = reportData["reportName"]
     projectNames  = reportData["projectNames"]
     inventoryData = reportData["inventoryData"]
-
-    print(projectNames)
     
     scriptDirectory = os.path.dirname(os.path.realpath(__file__))
     cssFile =  os.path.join(scriptDirectory, "html-assets/css/revenera_common.css")
@@ -78,8 +76,13 @@ def generate_html_report(reportData):
     html_ptr.write("        <meta charset='utf-8'>  \n")
     html_ptr.write("        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'> \n")
 
-    html_ptr.write(''' <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">\n''')
-  
+    html_ptr.write(''' 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    ''')
+
+
     html_ptr.write("        <style>\n")
 
     # Add the contents of the css file to the head block
@@ -92,6 +95,30 @@ def generate_html_report(reportData):
     except:
         logger.error("Unable to open %s" %cssFile)
         print("Unable to open %s" %cssFile)
+
+    # TODO Add to css file
+    html_ptr.write(" .tr-nomatch { background-color: #FFFFE0;}\n")
+    html_ptr.write(" .td-nomatch { color: #F80000;}\n")
+
+    html_ptr.write('''    
+    
+    .page-item.active .page-link {
+        color: #323E48 !important;
+        background-color: #1ED65F !important;
+        border-color: #323E48 !important; 
+    }
+
+    .page-link {
+        color: #323E48 !important;
+        background-color: #fff !important;
+        border: 1px solid #323E48 !important; 
+    }
+
+    .page-link:hover {
+        color: #1ED65F !important;
+        background-color: #323E48 !important;
+        border-color: #323E48 !important; 
+    }''')
     
     html_ptr.write("        </style>\n")  
 
@@ -120,16 +147,131 @@ def generate_html_report(reportData):
     html_ptr.write("        <hr class='small'>\n")
     html_ptr.write("<!-- END HEADER -->\n")
 
-
+    #---------------------------------------------------------------------------------------------------
+    # Body of Report
+    #---------------------------------------------------------------------------------------------------
     html_ptr.write("<!-- BEGIN BODY -->\n")  
-    html_ptr.write(" REPORT BODY\n")  
+
+    html_ptr.write('''<button id="hideNonExact">Show Common Components</button>''')
+    html_ptr.write('''<button id="hideExact">Show Differences</button>''')
+    
+    html_ptr.write('''<button id="reset">Show All</button>''')
+
+    html_ptr.write("<table id='comparisonData' class='table table-hover table-bordered table-sm' style='width:90%'>\n")
+
+    html_ptr.write("<colgroup>\n")
+    html_ptr.write("<col span=\"1\" style=\"width: 30%;\">\n")
+    html_ptr.write("<col span=\"1\" style=\"width: 20%;\">\n")
+    html_ptr.write("<col span=\"1\" style=\"width: 10%;\">\n")
+    html_ptr.write("<col span=\"1\" style=\"width: 20%;\">\n")
+    html_ptr.write("<col span=\"1\" style=\"width: 10%;\">\n")
+    html_ptr.write("</colgroup>\n")
+
+
+    html_ptr.write("    <thead>\n")
+    html_ptr.write("        <tr>\n")
+    html_ptr.write("            <th colspan='1' class='text-center'>&nbsp</th>\n")  
+    html_ptr.write("            <th colspan='2' class='text-center'>%s</th>\n" %projectNames[0])  
+    html_ptr.write("            <th colspan='2' class='text-center'>%s</th>\n" %projectNames[1]) 
+    html_ptr.write("        </tr>\n") 
+    html_ptr.write("        <tr>\n") 
+    html_ptr.write("            <th class='text-center'>Component</th>\n") 
+
+    html_ptr.write("            <th class='text-center'>Selected License</th>\n") 
+    html_ptr.write("            <th class='text-center'>Forge</th>\n")
+
+    html_ptr.write("            <th class='text-center'>Selected License</th>\n") 
+    html_ptr.write("            <th class='text-center'>Forge</th>\n")  
+    html_ptr.write("        </tr>\n")
+    html_ptr.write("    </thead>\n")  
+    html_ptr.write("    <tbody>\n")  
+
+
+    ######################################################
+    # Cycle through the inventory to create the 
+    # table with the results
+    for component in sorted(inventoryData):
+
+        # Is the component part of the first project?
+        if projectNames[0] in inventoryData[component].keys():
+            proj1_selectedLicenseName = inventoryData[component][projectNames[0]]['selectedLicenseName'] 
+            proj1_componentForgeName = inventoryData[component][projectNames[0]]['componentForgeName']
+        else:
+            proj1_selectedLicenseName = ""
+            proj1_componentForgeName = ""
+
+        # Is the component part of the second project?
+        if projectNames[1] in inventoryData[component].keys():
+            proj2_selectedLicenseName = inventoryData[component][projectNames[1]]['selectedLicenseName'] 
+            proj2_componentForgeName = inventoryData[component][projectNames[1]]['componentForgeName']
+        else:
+            proj2_selectedLicenseName = ""
+            proj2_componentForgeName = ""
+
+        ################################################
+        # Is this an exact match between projects?
+        if proj1_selectedLicenseName == proj2_selectedLicenseName and proj1_selectedLicenseName == proj2_selectedLicenseName and proj1_componentForgeName == proj2_componentForgeName:
+            matchType = "CVL"
+        else:
+            matchType = "CV"
+        
+        html_ptr.write("        <tr matchType='%s'> \n" %matchType)
+        html_ptr.write("            <td class='text-center'>%s</th>\n" %(component))
+        
+        if proj1_selectedLicenseName != proj2_selectedLicenseName and proj1_selectedLicenseName != "" and proj2_selectedLicenseName != "": 
+            html_ptr.write("            <td class='td-nomatch text-center'>%s</th>\n" %proj1_selectedLicenseName)
+        else:
+            html_ptr.write("            <td class='text-center'>%s</th>\n" %proj1_selectedLicenseName)
+        
+        if proj1_componentForgeName != proj2_componentForgeName and proj1_componentForgeName != "" and proj2_componentForgeName != "": 
+            html_ptr.write("            <td class='td-nomatch text-center'>%s</th>\n" %proj1_componentForgeName)
+        else:
+            html_ptr.write("            <td class='text-center'>%s</th>\n" %proj1_componentForgeName)
+
+        
+        if proj1_selectedLicenseName != proj2_selectedLicenseName and proj1_selectedLicenseName != "" and proj2_selectedLicenseName != "": 
+            html_ptr.write("            <td class='td-nomatch text-center'>%s</th>\n" %proj2_selectedLicenseName)
+        else:
+            html_ptr.write("            <td class='text-center'>%s</th>\n" %proj2_selectedLicenseName)
+
+        if proj1_componentForgeName != proj2_componentForgeName and proj1_componentForgeName != "" and proj2_componentForgeName != "": 
+            html_ptr.write("            <td class='td-nomatch text-center'>%s</th>\n" %proj2_componentForgeName)
+        else:
+            html_ptr.write("            <td class='text-center'>%s</th>\n" %proj2_componentForgeName)
+
+        html_ptr.write("        </tr>\n") 
+    html_ptr.write("    </tbody>\n")
+
+    '''
+    html_ptr.write("    <tfoot>\n")
+    html_ptr.write("        <tr>\n") 
+    html_ptr.write("            <th class='text-center'>Component</th>\n") 
+
+    html_ptr.write("            <th class='text-center'>Selected License</th>\n") 
+    html_ptr.write("            <th class='text-center'>Forge</th>\n")
+
+    html_ptr.write("            <th class='text-center'>Selected License</th>\n") 
+    html_ptr.write("            <th class='text-center'>Forge</th>\n")  
+    html_ptr.write("        </tr>\n")
+    
+    html_ptr.write("        <tr>\n")
+    html_ptr.write("            <th class='text-center'>&nbsp</th>\n")   
+    html_ptr.write("            <th colspan='2' class='text-center'>%s</th>\n" %projectNames[0])  
+    html_ptr.write("            <th colspan='2' class='text-center'>%s</th>\n" %projectNames[1]) 
+    html_ptr.write("        </tr>\n")
+    html_ptr.write("    </tfoot>\n")  
+    '''  
+
+    html_ptr.write("</table>\n")  
+
     html_ptr.write("<!-- END BODY -->\n")  
 
-
-
+    #---------------------------------------------------------------------------------------------------
+    # Report Footer
+    #---------------------------------------------------------------------------------------------------
     html_ptr.write("<!-- BEGIN FOOTER -->\n")  
     html_ptr.write("<hr class='small'>\n") 
-    html_ptr.write("<div style=\"height:35px;\">\n")
+    html_ptr.write("<div class='report-footer' style=\"height:35px;\">\n")
     html_ptr.write("    <div style=\"float:left\">\n")
     html_ptr.write("        &copy; 2020 Revenera.\n")
     html_ptr.write("    </div>\n")
@@ -144,9 +286,50 @@ def generate_html_report(reportData):
     #---------------------------------------------------------------------------------------------------
     # Add javascript 
     #---------------------------------------------------------------------------------------------------
-    html_ptr.write("<script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script>\n")
-    html_ptr.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\" integrity=\"sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN\" crossorigin=\"anonymous\"></script>\n")
-    html_ptr.write("<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js\" integrity=\"sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J\" crossorigin=\"anonymous\"></script>\n")    
+
+    html_ptr.write('''
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>  
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script> 
+
+    ''')
+
+
+    html_ptr.write('''
+        <script>
+            var table = $('#comparisonData').DataTable();
+
+            $("#hideExact").click(function() {
+                $.fn.dataTable.ext.search.pop();
+                $.fn.dataTable.ext.search.push(
+                function(settings, data, dataIndex) {
+                    return $(table.row(dataIndex).node()).attr('matchType') == "CV";
+                }
+            );
+            table.draw();
+            }); 
+
+
+            $("#hideNonExact").click(function() {
+            $.fn.dataTable.ext.search.pop();
+                $.fn.dataTable.ext.search.push(
+                function(settings, data, dataIndex) {
+
+                    return $(table.row(dataIndex).node()).attr('matchType') == "CVL";
+                    }
+                );
+                table.draw();
+            });    
+            $("#reset").click(function() {
+                $.fn.dataTable.ext.search.pop();
+                table.draw();
+            });
+            </script>
+
+
+        ''')
+
 
     html_ptr.write("</body>\n") 
     html_ptr.write("</html>\n") 
